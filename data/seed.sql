1b967c2464d0fd497b3e9bd4527ba086da8c263f
Chance Harmon <henrikillsit@gmail.com>
2020-03-25 16:33:07 -0700

    day 3 complete

--- data/seed.sql
+++ data/seed.sql
@@ -1,3 +1,16 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  author VARCHAR(255),
+  isbn VARCHAR(255),
+  image_url VARCHAR(255),
+  description TEXT,
+  bookshelf VARCHAR(255)
+);
+
+
 INSERT INTO books (title, author, isbn, image_url, description, bookshelf) VALUES (
   'Dune',
   'Frank Herbert',
